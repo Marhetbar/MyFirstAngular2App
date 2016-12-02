f228487a32a21bf0fd7f3380a5f691a4e583f60c
James <Marhetbar@gmail.com>
2016-12-02 14:41:36 -0500

    Movies component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesDirective } from './movies.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MoviesDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movies.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appMovies]'
+})
+export class MoviesDirective {
+
+  constructor() { }
+
+}
